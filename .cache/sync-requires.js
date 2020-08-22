adf2413f6c5925c3bb69dc2b325adf026b58027d
Lina Kaval <kavalla@mail.uc.edu>
2020-08-22 11:49:27 -0400

    new site

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Lina Kaval\\Documents\\Github Folder\\acm-w\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Lina Kaval\\Documents\\Github Folder\\acm-w\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\Lina Kaval\\Documents\\Github Folder\\acm-w\\src\\pages\\About.js"))),
+  "component---src-pages-get-involved-js": hot(preferDefault(require("C:\\Users\\Lina Kaval\\Documents\\Github Folder\\acm-w\\src\\pages\\GetInvolved.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Lina Kaval\\Documents\\Github Folder\\acm-w\\src\\pages\\index.js"))),
+  "component---src-pages-meetings-js": hot(preferDefault(require("C:\\Users\\Lina Kaval\\Documents\\Github Folder\\acm-w\\src\\pages\\Meetings.js"))),
+  "component---src-pages-resources-js": hot(preferDefault(require("C:\\Users\\Lina Kaval\\Documents\\Github Folder\\acm-w\\src\\pages\\Resources.js")))
+}
+
